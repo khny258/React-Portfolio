b9c0ba70a81b24e173a6d6e0b5aeb1af1c354787
Kenny Huang <khny258@gmail.com>
2020-07-27 21:54:50 -0400

    updated about me page

--- pages/about.jsx
+++ pages/about.jsx
@@ -17,9 +17,9 @@ const App = () => (
       </div>
       <div className="col-md-6 aboutSections">
         <h4 className="bio bio1">
-        Full Stack Developer leveraging my 10+ years of experience in the finance industry to build useful applications and exciting things that will directly impact and change lives. After several years in Finance, I realized I had a love for coding so I enrolled in Columbia University where I became proficient in JavaScript, CSS, responsive web design, APIs, MERN Stack (MongoDB, Express.js, React.js, Node.js), and MySQL.
+        <p>Full Stack Developer leveraging my 10+ years of experience in the finance industry to build useful applications and exciting things that will directly impact and change lives. After several years in Finance, I realized I had a love for coding so I enrolled in Columbia University where I became proficient in JavaScript, CSS, responsive web design, APIs, MERN Stack (MongoDB, Express.js, React.js, Node.js), and MySQL.</p>
         
-        I can develop innovative applications such as a group project called Timbr, a harmonious dating service for musicians, we completed involving a Node and Express web server backed by a MySQL database with a Sequelize ORM. My technical skills combined with my experience in collaborative, deadline-driven environments make me a strong addition to any web development team.
+        <p>I can develop innovative applications such as a group project called Edward, a smarter version of the SEC’s EDGAR system, made with React, Puppeteer/Cheerio for scraping data, and React-VIS for charting data. My technical skills combined with my experience in collaborative, deadline-driven environments make me a strong addition to any web development team.</p>
         </h4>
       </div>
     </div>
