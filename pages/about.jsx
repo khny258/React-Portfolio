be71bb3e472f0722f8df03bc1be322b86ddd1e10
Kenny Huang <khny258@gmail.com>
2020-07-28 20:36:36 -0400

    reduced my skills image sizes

--- pages/about.jsx
+++ pages/about.jsx
@@ -29,14 +29,14 @@ const App = () => (
       <div className="col">
         <div className="row">
           <div className="col-sm-12">
-            <IconImage source="../static/pics/html.png" alt="html" />
-            <IconImage source="../static/pics/css.png" alt="css" />
-            <IconImage source="../static/pics/bootstrap.png" alt="bootstrap" />
+            <IconImage source="../static/pics/html.png" alt="html" width="50%" height="50%" />
+            <IconImage source="../static/pics/css.png" alt="css" width="50%" height="50%" />
+            <IconImage source="../static/pics/bootstrap.png" alt="bootstrap" width="50%" height="50%" />
           </div>
           <div className="col-sm-12">
-            <IconImage source="../static/pics/javascript.png" alt="javascript" />
-            <IconImage source="../static/pics/handlebars.png" alt="handlebars" />
-            <IconImage source="../static/pics/jquery.png" alt="jquery" />
+            <IconImage source="../static/pics/javascript.png" alt="javascript" width="50%" height="50%" />
+            <IconImage source="../static/pics/handlebars.png" alt="handlebars" width="50%" height="50%" />
+            <IconImage source="../static/pics/jquery.png" alt="jquery" width="50%" height="50%" />
           </div>
         </div>
       </div>
@@ -45,14 +45,14 @@ const App = () => (
       <div className="col">
         <div className="row">
           <div className="col-sm-12">
-            <IconImage source="../static/pics/githubIcon.png" alt="github" /> 
-            <IconImage source="../static/pics/mern.jpg" alt="mern" />
-            <IconImage source="../static/pics/mongodb.png" alt="mongodb" />
+            <IconImage source="../static/pics/githubIcon.png" alt="github" width="50%" height="50%" /> 
+            <IconImage source="../static/pics/mern.jpg" alt="mern" width="50%" height="50%" />
+            <IconImage source="../static/pics/mongodb.png" alt="mongodb" width="50%" height="50%" />
           </div>
           <div className="col-sm-12">
-            <IconImage source="../static/pics/mysql.png" alt="mysql" />
-            <IconImage source="../static/pics/nodejs.png" alt="node" />
-            <IconImage source="../static/pics/React.png" alt="react" />
+            <IconImage source="../static/pics/mysql.png" alt="mysql" width="50%" height="50%" />
+            <IconImage source="../static/pics/nodejs.png" alt="node" width="50%" height="50%" />
+            <IconImage source="../static/pics/React.png" alt="react" width="50%" height="50%" />
           </div>
         </div>
       </div>
