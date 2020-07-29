612211fd286394cbbd467fab4c2762c770bf0b76
Kenny Huang <khny258@gmail.com>
2020-07-28 20:22:48 -0400

    updated about page

--- pages/about.jsx
+++ pages/about.jsx
@@ -28,16 +28,28 @@ const App = () => (
     <div className="row">
       <div className="col">
         <div className="row">
-          <div className="col-sm-6">
+          <div className="col-sm-3">
             <IconImage source="../static/pics/html.png" alt="html" />
-            <IconImage source="../static/pics/bootstrap.png" alt="bootstrap" />
             <IconImage source="../static/pics/css.png" alt="css" />
-            <IconImage source="../static/pics/githubIcon.png" alt="github" />
-            <IconImage source="../static/pics/handlebars.png" alt="handlebars" />
+            <IconImage source="../static/pics/bootstrap.png" alt="bootstrap" />
+          </div>
+          <div className="col-sm-3">
             <IconImage source="../static/pics/javascript.png" alt="javascript" />
+            <IconImage source="../static/pics/handlebars.png" alt="handlebars" />
             <IconImage source="../static/pics/jquery.png" alt="jquery" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="row">
+      <div className="col">
+        <div className="row">
+          <div className="col-sm-3">
+            <IconImage source="../static/pics/githubIcon.png" alt="github" /> 
             <IconImage source="../static/pics/mern.jpg" alt="mern" />
             <IconImage source="../static/pics/mongodb.png" alt="mongodb" />
+          </div>
+          <div className="col-sm-3">
             <IconImage source="../static/pics/mysql.png" alt="mysql" />
             <IconImage source="../static/pics/nodejs.png" alt="node" />
             <IconImage source="../static/pics/React.png" alt="react" />
