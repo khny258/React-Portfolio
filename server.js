164b19dbf012b5d397896be0144e56256cb88623
Kenny Huang <khny258@gmail.com>
2020-08-12 20:32:11 -0400

    added updated resume links

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
-const next = require('next');
-const express = require('express');
+const next = require("next");
+const express = require("express");
 const PORT = process.env.PORT || 3000;
-const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const server = express();
-const viewRoutes = require('./viewRoutes');
+const viewRoutes = require("./viewRoutes");
 
-app.prepare().then(() => {
-  viewRoutes(server, app);
+app.prepare()
+    .then(() => {
+        viewRoutes(server, app);
 
-  // Renders home page for wildcard routes
-  server.get('*', (req, res) => {
-    app.render(req, res, '/');
-  });
+        // Renders home page for wildcard routes
+        server.get("*", (req, res) => {
+            app.render(req, res, "/");
+        });
 
-  server.listen(PORT, (err) => {
-    if (err) throw err;
-    // eslint-disable-next-line no-console
-    console.log('[ i ] Next.js server ready on port', PORT);
-  });
-}).catch((ex) => {
-  console.error(ex.stack); // eslint-disable-line no-console
-  process.exit(1);
-});
-
+        server.listen(PORT, (err) => {
+            if (err) throw err;
+            // eslint-disable-next-line no-console
+            console.log("[ i ] Next.js server ready on port", PORT);
+        });
+    })
+    .catch((ex) => {
+        console.error(ex.stack); // eslint-disable-line no-console
+        process.exit(1);
+    });
 
 module.exports = app;
--- viewRoutes.js
+++ viewRoutes.js
@@ -1,31 +1,31 @@
 module.exports = (server, app) => {
   // fixes error where about page isnt rendered with extra forward slash
-  server.get('/about/', (req, res) => {
-    app.render(req, res, '/about');
+  server.get("/about/", (req, res) => {
+      app.render(req, res, "/about");
   });
 
   // renders about page no matter what is after
-  server.get('/about/:page', (req, res) => {
-    app.render(req, res, '/about');
+  server.get("/about/:page", (req, res) => {
+      app.render(req, res, "/about");
   });
 
   // fixes error where about page isnt rendered with extra forward slash
-  server.get('/about', (req, res) => {
-    app.render(req, res, '/about');
+  server.get("/about", (req, res) => {
+      app.render(req, res, "/about");
   });
 
   // fixes error where contact page isnt rendered with extra forward slash
-  server.get('/contact/', (req, res) => {
-    app.render(req, res, '/contact');
+  server.get("/contact/", (req, res) => {
+      app.render(req, res, "/contact");
   });
 
   // renders contact page no matter what is after
-  server.get('/contact/:page', (req, res) => {
-    app.render(req, res, '/contact');
+  server.get("/contact/:page", (req, res) => {
+      app.render(req, res, "/contact");
   });
 
   // fixes error where contact page isnt rendered with extra forward slash
-  server.get('/contact', (req, res) => {
-    app.render(req, res, '/contact');
+  server.get("/contact", (req, res) => {
+      app.render(req, res, "/contact");
   });
 };
