8b8536af6b8c95ba9e87ecf78db3a4c55cd4ccb8
Kenny Huang <khny258@gmail.com>
2020-09-13 13:24:01 -0400

    updated index & about

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -25,6 +25,10 @@ class MainApp extends App {
             integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
             crossOrigin="anonymous"
           />
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+          />
           <script
             src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
             integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
--- pages/index.jsx
+++ pages/index.jsx
@@ -12,15 +12,19 @@ const Home = () => (
     <h3>My Projects</h3>
     <hr />
     <div className="row">
-      <div className="col-md-4">
+    <div className="col-md-1" class="animate__animated animate__bounceInDown"> 
+    </div>
+      <div className="col-md-3" class="animate__animated animate__bounceInDown">
         <Picture source="../static/pics/b.jpg" alt="Edward" deployed="https://edward-2.herokuapp.com/" />
       </div>
-      <div className="col-md-4">
+      <div className="col-md-3" class="animate__animated animate__bounceInDown">
         <Picture source="../static/pics/a.jpg" alt="Rain or Shine" deployed="https://khny258.github.io/Rain-or-Shine/" />
       </div>
-      <div className="col-md-4">
+      <div className="col-md-3" class="animate__animated animate__bounceInDown">
         <Picture source="../static/pics/c.jpg" alt="Timbr" deployed="https://khny258.github.io/Timbr/" />
       </div>
+      <div className="col-md-1" class="animate__animated animate__bounceInDown"> 
+    </div>
     </div>
     <Footer />
   </div>
