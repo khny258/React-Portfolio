4e1c60db1039d7bb21a05be0024595f21bd2d91b
Kenny Huang <khny258@gmail.com>
2020-10-21 13:42:46 -0400

    added bitcoin project

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,19 +12,19 @@ const Home = () => (
     <h3>My Projects</h3>
     <hr />
     <div className="row">
-    <div className="col-md-1"> 
-    </div>
-      <div className="col-md-3" class="animate__animated animate__backInLeft">
+      
+      <div className="col-md-2" class="animate__animated animate__backInLeft">
         <Picture source="../static/pics/b.jpg" alt="Edward" deployed="https://edward-2.herokuapp.com/" />
       </div>
-      <div className="col-md-3" class="animate__animated animate__backInUp">
+      <div className="col-md-2" class="animate__animated animate__backInUp">
         <Picture source="../static/pics/a.jpg" alt="Rain or Shine" deployed="https://khny258.github.io/Rain-or-Shine/" />
       </div>
-      <div className="col-md-3" class="animate__animated animate__backInRight">
+      <div className="col-md-2" class="animate__animated animate__backInRight">
         <Picture source="../static/pics/c.jpg" alt="Timbr" deployed="https://khny258.github.io/Timbr/" />
       </div>
-      <div className="col-md-1"> 
-    </div>
+      <div className="col-md-2" class="animate__animated animate__backInRight">
+        <Picture source="../static/pics/btc.png" alt="BTC" deployed="https://khny258.github.io/Bitcoin/" />
+      </div>
     </div>
     <Footer />
   </div>
